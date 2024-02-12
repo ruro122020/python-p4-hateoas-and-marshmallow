5b71dd8ccee09deec7f98e0cc47eb54bc9a2a36f
Ruth <ruro122020@gmail.com>
2024-02-11 22:12:23 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -15,6 +15,28 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
+ma = Marshmallow(app)
+
+class NewsletterSchema(ma.SQLAlchemyAutoSchema):
+    
+    class Meta:
+        model = Newsletter
+        load_instance = True
+    title = ma.auto_field()
+    published_at =  ma.auto_field()
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "newsletterbyid",
+                values=dict(id="<id>")),
+            "collection": ma.URLFor("newsletters"),
+        }
+    )
+
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
+
 api = Api(app)
 
 class Index(Resource):
@@ -41,7 +63,7 @@ class Newsletters(Resource):
         response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
 
         response = make_response(
-            response_dict_list,
+            newsletters_schema.dump(response_dict_list),
             200,
         )
 
@@ -116,7 +138,6 @@ class NewsletterByID(Resource):
         return response
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
